fc919eef7b1d458cbca09e04821c7b4cb635d737
Swastik Giri <swastikrancodes@gmail.com>
2024-06-19 13:41:35 +0530

    serverjs created

---
+++ src/servers.js
@@ -0,0 +1,22 @@
+export const servers = [
+  {
+    name: "Thinkzone Production",
+    adr: "https://thinkzone.co/thinkzone",
+    icon: "fa fa-globe",
+  },
+  {
+    name: "Prakashak Production",
+    adr: "https://tatvagyan.co.in/",
+    icon: "fa fa-globe",
+  },
+  {
+    name: "Thinkzone Test",
+    adr: "https://thinkzone.in.net/thinkzone",
+    icon: "fa fa-server",
+  },
+  {
+    name: "Prakashak Test",
+    adr: "https://tatvagyan.in/",
+    icon: "fa fa-server",
+  },
+];
